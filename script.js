4e32281e6cba1b64f8a12ac46c99c95140087fdc
Krzysztof <111344564+Krzysztof1412@users.noreply.github.com>
2022-09-09 21:04:31 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 'use strict';
 
+const form = document.querySelector('form');
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const email = document.querySelector('#email');
+  const message = document.querySelector('#message');
+  const info = document.querySelector('#form-info');
+
+  let re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (!email.value && !message.value) {
+    info.textContent =
+      'Uzupełnij poprawnie adres e-mail oraz nie zapomnij napisać wiadomości.';
+    return;
+  }
+  if (!email.value || !re.test(email.value)) {
+    info.textContent = 'Uzupełnij poprawnie adres e-mail.';
+    return;
+  }
+  if (!message.value) {
+    info.textContent = 'Napisz wiadomość aby kontynuować.';
+    return;
+  }
+
+  form.submit();
+});
+
 const appendElements = function () {
   const h1 = document.querySelector('.start-center h1');
   const h2 = document.querySelector('.start-center h2');
@@ -9,5 +36,26 @@ const appendElements = function () {
   setTimeout(() => img.classList.remove('hidden'), 1500);
   setTimeout(() => h2.classList.remove('hidden'), 2500);
 };
-
 appendElements();
+
+// const addObervers = function () {
+//   const moveToSection = function (entries) {
+//     const [entry] = entries;
+//     const coords = entry.target.getBoundingClientRect().top;
+
+//     if (entry.isIntersecting) {
+//       const windowHeight = window.innerHeight;
+//       entry.target.scrollIntoView({ behavior: 'smooth' });
+//       // window.scrollBy(0, windowHeight - window.scrollY);
+//       console.log(entry);
+//     }
+//   };
+
+//   const aboutMe = document.querySelector('.about-me-section');
+
+//   const sectionObserver = new IntersectionObserver(moveToSection, {
+//     rootMargin: '-100px',
+//   });
+//   sectionObserver.observe(aboutMe);
+// };
+// addObervers();
