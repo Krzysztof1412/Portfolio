9c75d723b07fadb3c550fc62a42e41bcacd134eb
Krzysztof <111344564+Krzysztof1412@users.noreply.github.com>
2022-09-28 12:37:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ const appendElements = function () {
   const img = document.querySelector('.start-tile img');
 
   setTimeout(() => h1.classList.remove('hidden'), 500);
-  setTimeout(() => img.classList.remove('hidden'), 3500);
+  setTimeout(() => img.classList.remove('hidden'), 2500);
   setTimeout(() => h2.classList.remove('hidden'), 1500);
 };
 appendElements();
